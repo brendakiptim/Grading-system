2ee80749ff2eb3bee539469431785980ffa92347
Brenda Kiptim <93176526+brendakiptim@users.noreply.github.com>
2021-11-02 20:17:35 +0300

    added files

---
+++ gradeCalculator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+// above 70 is A
+// 60 to 69 is B
+// 50 to 59 is C
+// 40 to 49 is D
+// below 40 is F
+{
+    string name = "";
+    float grade = 0;
+
+    cout << "Grading System\n";
+    cout << "Please enter the name of the student:\n";
+    getline(cin, name);
+    cout << "Please enter the student's grades:\n";
+    cin >> grade;
+
+    if (grade >= 70)
+    {
+        cout << name << " has scored an A\n";
+    }
+    else if (grade >= 69)
+    {
+        cout << name << " has scored an B\n";
+    }
+    else if (grade >= 59)
+    {
+        cout << name << " has scored an C\n";
+    }
+    else if (grade >= 49)
+    {
+        cout << name << " has scored an D\n";
+    }
+    else if (grade >= 40)
+    {
+        cout << name << " has scored an F\n";
+    }
+    else
+    {
+        cout << "error try again\n";
+    }
+    return 0;
+}
